a5dbc33287f150c8614ab1a854ac91b3ed2cbc24
SwiftSeal <65286772+SwiftSeal@users.noreply.github.com>
2023-11-03 15:57:22 +0000

    fixes

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,7 +3,7 @@
 #SBATCH -J pangenomics
 #SBATCH -p long
 #SBATCH -c 1
-#SBATCH --mem=8G
+#SBATCH --mem=4G
 #SBATCH --export=ALL
 #SBATCH -o snakemake.%j.out
 #SBATCH -e snakemake.%j.err
--- workflow/scripts/genespace.R
+++
@@ -1,8 +0,0 @@
-devtools::install_github("jtlovell/GENESPACE")
-
-library(GENESPACE)
-
-# get mcscanx path from args
-path2mcscanx <- commandArgs(trailingOnly = TRUE)[1]
-
-gpar <- init_genespace(wd = "results/", path2mcscanx = path2mcscanx)
